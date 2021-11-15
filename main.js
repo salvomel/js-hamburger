05c822d54f3a4c58fbd3fbbb84965bc07d997042
salvomel <salv.mel@gmail.com>
2021-11-15 16:13:07 +0100

    done

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const rightMenu = document.getElementById('header-right');
-const burgerMenu = document.getElementById('hamburger-menu');
 
-rightMenu.addEventListener('click',
-    function() {
-        
-        
+const burgerButton = document.querySelector('.header-right > a');
+
+burgerButton.addEventListener('click', 
+    function () {
+        const burgerMenu = document.querySelector('.hamburger-menu');
+        burgerMenu.style.display = 'block'
     }
 )
+
+const closeMenu = document.querySelector('.close');
+
+closeMenu.addEventListener('click',
+    function () {
+        const burgerMenu = document.querySelector('.hamburger-menu');
+        burgerMenu.style.display = 'none'
+    }
+)
+
+
