e992fac385f6b58a163b2fcda977914cfe2233f5
salvomel <salv.mel@gmail.com>
2021-11-15 15:41:20 +0100

    repo

---
+++ main.js
@@ -0,0 +1,9 @@
+const rightMenu = document.getElementById('header-right');
+const burgerMenu = document.getElementById('hamburger-menu');
+
+rightMenu.addEventListener('click',
+    function() {
+        
+        
+    }
+)
